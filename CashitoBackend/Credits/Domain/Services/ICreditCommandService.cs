d1922174d323755273de0406e47a86e7bb1de482
Elandrehs <andrestheb@gmail.com>
2026-04-21 23:32:54 -0500

    feat(credits): add application services, simulation service, repository and db config

--- CashitoBackend/Credits/Domain/Services/ICreditCommandService.cs
+++ CashitoBackend/Credits/Domain/Services/ICreditCommandService.cs
@@ -1,21 +1,14 @@
-﻿using CashitoBackend.Credits.Domain.Model.Aggregates;
+﻿using CashitoBackend.Credits.Application.Internal.DTOs;
+using CashitoBackend.Credits.Domain.Model.Aggregates;
 using CashitoBackend.Credits.Domain.Model.Commands;
 
 namespace CashitoBackend.Credits.Domain.Services;
 
 public interface ICreditCommandService
 {
-    // 🔥 SIMULACIÓN (NO GUARDA)
-    Task<(decimal cuota,
-            decimal tcea,
-            decimal van,
-            decimal tir,
-            IEnumerable<object> schedule)>
-        Handle(SimulateCreditCommand command, int userId);
+    Task<SimulationResult> Handle(SimulateCreditCommand command, int userId);
 
-    // 💾 CREAR CRÉDITO
     Task<Credit> Handle(CreateCreditCommand command, int userId);
 
-    // 🗑️ OPCIONAL
     Task<bool> Delete(int creditId, int userId);
 }
