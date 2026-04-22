2d2972f1a84ed7c78dacb4e01cf64fefaf764ef5
Elandrehs <andrestheb@gmail.com>
2026-04-21 21:45:58 -0500

    feat(clients): add application services (command and query)

---
+++ CashitoBackend/Clients/Application/Application/QueryServices/ClientQueryService.cs
@@ -0,0 +1,34 @@
+﻿using CashitoBackend.Clients.Domain.Model.Aggregates;
+using CashitoBackend.Clients.Domain.Model.Queries;
+using CashitoBackend.Clients.Domain.Repositories;
+using CashitoBackend.Clients.Domain.Services;
+
+namespace CashitoBackend.Clients.Application.Application.QueryServices;
+
+public class ClientQueryService : IClientQueryService
+{
+    private readonly IClientRepository _clientRepository;
+
+    public ClientQueryService(IClientRepository clientRepository)
+    {
+        _clientRepository = clientRepository;
+    }
+    
+    public async Task<IEnumerable<Client>> Handle(GetAllClientsQuery query)
+    {
+        return await _clientRepository.FindByUserIdAsync(query.UserId);
+    }
+    
+    public async Task<Client?> Handle(GetClientByIdQuery query)
+    {
+        var client = await _clientRepository.FindByIdAsync(query.Id);
+
+        if (client == null)
+            return null;
+
+        if (client.UserId != query.UserId)
+            return null;
+
+        return client;
+    }
+}
