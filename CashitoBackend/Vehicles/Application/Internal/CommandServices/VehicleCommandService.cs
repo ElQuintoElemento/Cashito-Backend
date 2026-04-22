2283d3af15f12da6a1ab6f8f59a1618ca8d8660c
Elandrehs <andrestheb@gmail.com>
2026-04-21 22:42:46 -0500

    feat(vehicles): add application services (command and query)

---
+++ CashitoBackend/Vehicles/Application/Internal/CommandServices/VehicleCommandService.cs
@@ -0,0 +1,80 @@
+﻿using CashitoBackend.Shared.Domain.Repositories;
+using CashitoBackend.Vehicles.Domain.Model.Aggregates;
+using CashitoBackend.Vehicles.Domain.Model.Commands;
+using CashitoBackend.Vehicles.Domain.Repositories;
+using CashitoBackend.Vehicles.Domain.Services;
+
+namespace CashitoBackend.Vehicles.Application.Internal.CommandServices;
+
+public class VehicleCommandService : IVehicleCommandService
+{
+    private readonly IVehicleRepository _vehicleRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public VehicleCommandService(
+        IVehicleRepository vehicleRepository,
+        IUnitOfWork unitOfWork)
+    {
+        _vehicleRepository = vehicleRepository;
+        _unitOfWork = unitOfWork;
+    }
+    
+    public async Task<Vehicle> Handle(CreateVehicleCommand command, int userId)
+    {
+        var vehicle = new Vehicle(
+            userId,
+            command.Brand,
+            command.Model,
+            command.Price,
+            command.Currency,
+            command.Year,
+            command.Type
+        );
+
+        await _vehicleRepository.AddAsync(vehicle);
+        await _unitOfWork.CompleteAsync();
+
+        return vehicle;
+    }
+    
+    public async Task<Vehicle?> Handle(UpdateVehicleCommand command, int userId)
+    {
+        var vehicle = await _vehicleRepository.FindByIdAsync(command.Id);
+
+        if (vehicle == null)
+            return null;
+
+        if (vehicle.UserId != userId)
+            throw new UnauthorizedAccessException("Not allowed");
+
+        vehicle.Update(
+            command.Brand,
+            command.Model,
+            command.Price,
+            command.Currency,
+            command.Year,
+            command.Type
+        );
+
+        _vehicleRepository.Update(vehicle);
+        await _unitOfWork.CompleteAsync();
+
+        return vehicle;
+    }
+    
+    public async Task<bool> Handle(DeleteVehicleCommand command, int userId)
+    {
+        var vehicle = await _vehicleRepository.FindByIdAsync(command.Id);
+
+        if (vehicle == null)
+            return false;
+
+        if (vehicle.UserId != userId)
+            throw new UnauthorizedAccessException("Not allowed");
+
+        _vehicleRepository.Remove(vehicle);
+        await _unitOfWork.CompleteAsync();
+
+        return true;
+    }
+}
