4b701cc4504b1885a8165dc5a48ecf35073e8d4a
Elandrehs <andrestheb@gmail.com>
2026-04-21 22:35:21 -0500

    feat(vehicles): add domain layer (aggregate, commands, queries, value objects, events)

---
+++ CashitoBackend/Vehicles/Domain/Model/Commands/CreateVehicleCommand.cs
@@ -0,0 +1,10 @@
+﻿namespace CashitoBackend.Vehicles.Domain.Model.Commands;
+
+public record CreateVehicleCommand(
+    string Brand,
+    string Model,
+    decimal Price,
+    string Currency,
+    int Year,
+    string Type
+);
